97101485ab088fcbad8a1ba78c4dcbc85440ff7f
OSCAR072021 <90817690+OSCAR072021@users.noreply.github.com>
2021-09-23 14:48:37 -0400

    Implementacion clientes guardar buscar lista

---
+++ application/models/Cliente_model.php
@@ -0,0 +1,50 @@
+<?php
+ 
+class Cliente_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+
+    function getCliente($idcliente)
+    {
+        return $this->db->get_where('persona',array('idpersona'=>$idpersona))->row_array();
+    }
+    
+
+    function get_allcliente_count()
+    {
+        $this->db->from('persona');
+        return $this->db->count_all_results();
+    }
+        
+
+    function get_allcliente()
+    {
+        $this->db->order_by('idpersona', 'desc');       
+        $this->db->where('tipo_persona', 'Cliente');  
+        return $this->db->get('persona')->result_array();
+    }
+        
+
+    function addcliente($params)
+    {
+        $this->db->insert('persona',$params);
+        return $this->db->insert_id();
+    }
+    
+
+    function updatecliente($idcliente,$params)
+    {
+        $this->db->where('idpersona',$idcliente);
+        return $this->db->update('persona',$params);
+    }
+    
+    function cambiarEstado($idcliente,$params)
+    {
+        $this->db->where('idpersona',$idcliente);
+        return $this->db->update('persona',$params);
+    }
+}
---
+++ application/views/cliente/add.php
@@ -0,0 +1,67 @@
+<div class="main-panel">
+
+  <div class="content-wrapper">
+    <div class="page-header">
+      <h3 class="page-title">
+        Bienvenido
+      </h3>
+      <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+	            <li class="breadcrumb-item"><a href="<?php echo site_url('welcome'); ?>">Inicio</a></li>
+	            <li class="breadcrumb-item"><a href="<?php echo site_url('cliente'); ?>">Lista Clientes</a></li>
+	            <li class="breadcrumb-item active" aria-current="page">Nuevo Cliente</li>
+            </ol>
+      </nav>
+    </div>
+    <div class="row grid-margin">
+      	<div class="col-12">
+          <?php
+          if (!empty($mensaje)) {
+          ?>
+            <div class="alert alert-danger">
+             El cliente se encuentra registrado
+           </div>
+          <?php } ?>
+    		  <?php echo form_open(base_url().'cliente/guardarDB',array("class"=>"forms-sample","enctype"=>"multipart/form-data")); ?>
+    				        <div class="form-group row">
+                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Nombre Completo</label>
+                      <div class="col-sm-4">
+                        <input type="hidden" class="form-control" name="tipo_persona" value="Cliente">
+                        <input type="text" class="form-control" name="nombre" placeholder="Nombre Completo">
+                        <span class="text-danger"><?php echo form_error('nombre');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">CI</label>
+                      <div class="col-sm-5">
+                        <input type="text" class="form-control" name="num_documento" placeholder="Ci">
+                        <span class="text-danger"><?php echo form_error('num_documento');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Dirección</label>
+                      <div class="col-sm-5">
+                        <input type="text" class="form-control" name="direccion" placeholder="Dirección">
+                        <span class="text-danger"><?php echo form_error('direccion');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Telefono</label>
+                      <div class="col-sm-4">
+                        <input type="text" class="form-control" name="telefono" placeholder="Telefono">
+                        <span class="text-danger"><?php echo form_error('telefono');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Email</label>
+                      <div class="col-sm-5">
+                        <input type="email" class="form-control" name="email" placeholder="Email">
+                        <span class="text-danger"><?php echo form_error('email');?></span>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-2">Guardar</button>
+                    <a href="<?php echo site_url('categoria'); ?>" class="btn btn-light">Cancelar</a>
+    		<?php echo form_close(); ?>
+      	</div>
+    </div>
+  </div> <!-- content wrapper -->
