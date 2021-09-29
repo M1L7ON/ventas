ec3f3e536b87127ec07f9280c2e46e73d649dbb8
Oscar Lopez <fheer_22@msn.com>
2021-09-29 16:17:35 -0400

    Implementacion Editar cliente, comentarios en Cliente controller y Cliente_model

--- application/models/Cliente_model.php
+++ application/models/Cliente_model.php
@@ -1,50 +1,99 @@
 <?php
- 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Clase Cliente_model
+ *
+ * @autor Oscar Perez
+ * @autor Oscar Lopez
+ * @version 1.0
+ */
 class Cliente_model extends CI_Model
 {
+    /**
+     * Cliente_model constructor.
+     */
     function __construct()
     {
         parent::__construct();
     }
-    
 
+    /**
+     * GetCliente.
+     * @param $idcliente Id del Cliente.
+     * @return row_array con datos de un cliente.
+     */
     function getCliente($idcliente)
     {
-        return $this->db->get_where('persona',array('idpersona'=>$idpersona))->row_array();
+        return $this->db->get_where('persona',array('idpersona'=>$idcliente))->row_array();
     }
-    
 
-    function get_allcliente_count()
+    /**
+     * GetAllClienteCount.
+     * @return entero Cantidad de clientes.
+     */
+    function getAllClienteCount()
     {
         $this->db->from('persona');
         return $this->db->count_all_results();
     }
-        
 
-    function get_allcliente()
+    /**
+     * GetAllCliente.
+     * @return result_array con datos de varios clientes.
+     */
+    function getAllCliente()
     {
         $this->db->order_by('idpersona', 'desc');       
         $this->db->where('tipo_persona', 'Cliente');  
         return $this->db->get('persona')->result_array();
     }
-        
 
-    function addcliente($params)
+    /**
+     * AddCliente.
+     * @param $params Datos de los campos de la base de datos y su valor a guardar.
+     * @return Id del ultimos cliente guardado.
+     */
+    function addCliente($params)
     {
         $this->db->insert('persona',$params);
         return $this->db->insert_id();
     }
-    
 
-    function updatecliente($idcliente,$params)
+    /**
+     * AddCliente
+     * @param $idcliente Id del cliente.
+     * @param $params Datos de los campos de la base de datos y su valor a modificar.
+     * @return Id del cliente modificado
+     */
+    function updateCliente($idcliente,$params)
     {
         $this->db->where('idpersona',$idcliente);
         return $this->db->update('persona',$params);
     }
-    
+
+    /**
+     * @param $idcliente Id del cliente.
+     * @param $params condicion 1 o 0.
+     * @return bool.
+     */
     function cambiarEstado($idcliente,$params)
     {
         $this->db->where('idpersona',$idcliente);
         return $this->db->update('persona',$params);
+    }
+
+    /**
+     * getClientesReporte.
+     * @return result con idpersona, nombre, direccion, telefono, email.
+     */
+    public function getClientesReporte(){
+        $this->db->select('idpersona, nombre, direccion, telefono, email');
+        $this->db->from('persona');
+        $this->db->Where('tipo_persona','Cliente');
+        $this->db->order_by('nombre','asc');
+
+        $r = $this->db->get();
+        return $r->result();
     }
 }
---
+++ application/views/cliente/edit.php
@@ -0,0 +1,68 @@
+<div class="main-panel">
+
+  <div class="content-wrapper">
+    <div class="page-header">
+      <h3 class="page-title">
+        Bienvenido
+      </h3>
+      <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+	            <li class="breadcrumb-item"><a href="<?php echo site_url('welcome'); ?>">Inicio</a></li>
+	            <li class="breadcrumb-item"><a href="<?php echo site_url('cliente'); ?>">Lista Clientes</a></li>
+	            <li class="breadcrumb-item active" aria-current="page">Editar Cliente</li>
+            </ol>
+      </nav>
+    </div>
+    <div class="row grid-margin">
+      	<div class="col-12">
+          <?php
+          if (!empty($mensaje)) {
+          ?>
+            <div class="alert alert-danger">
+             El cliente No se encuentra registrado
+           </div>
+          <?php } ?>
+    		  <?php echo form_open(base_url().'cliente/editarDB',array("class"=>"forms-sample","enctype"=>"multipart/form-data")); ?>
+    				        <div class="form-group row">
+                    <label for="nombre" class="col-sm-3 col-form-label">Nombre Completo</label>
+                      <div class="col-sm-4">
+                        <input type="hidden" class="form-control" name="idpersona" value="<?php echo $cliente['idpersona']; ?>">
+                        <input type="hidden" class="form-control" name="tipo_persona" value="<?php echo $cliente['tipo_persona']; ?>">
+                        <input type="text" class="form-control" name="nombre" value="<?php echo $cliente['nombre']; ?>">
+                        <span class="text-danger"><?php echo form_error('nombre');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="num_documento" class="col-sm-3 col-form-label">CI</label>
+                      <div class="col-sm-5">
+                        <input type="text" class="form-control" name="num_documento" value="<?php echo $cliente['num_documento']; ?>">
+                        <span class="text-danger"><?php echo form_error('num_documento');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="direccion" class="col-sm-3 col-form-label">Dirección</label>
+                      <div class="col-sm-5">
+                        <input type="text" class="form-control" name="direccion" value="<?php echo $cliente['direccion']; ?>">
+                        <span class="text-danger"><?php echo form_error('direccion');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="telefono" class="col-sm-3 col-form-label">Telefono</label>
+                      <div class="col-sm-4">
+                        <input type="text" class="form-control" name="telefono" value="<?php echo $cliente['telefono']; ?>">
+                        <span class="text-danger"><?php echo form_error('telefono');?></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="email" class="col-sm-3 col-form-label">Email</label>
+                      <div class="col-sm-5">
+                        <input type="email" class="form-control" name="email" value="<?php echo $cliente['email']; ?>">
+                        <span class="text-danger"><?php echo form_error('email');?></span>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-2">Guardar</button>
+                    <a href="<?php echo site_url('categoria'); ?>" class="btn btn-light">Cancelar</a>
+    		<?php echo form_close(); ?>
+      	</div>
+    </div>
+  </div> <!-- content wrapper -->
